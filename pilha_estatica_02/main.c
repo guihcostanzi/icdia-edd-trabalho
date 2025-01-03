d9abc67126514c32bee48a68cf802ba86e129835
Guilherme Costanzi Do <guilherme.costanzi@genesisgroup.com.br>
2025-01-03 20:48:37 -0300

    Segunda parte do trabalho concluída. Pilha estática com exibição invertida.

--- pilha_estatica_02/main.c
+++ pilha_estatica_02/main.c
@@ -3,9 +3,25 @@
 #include <stdio.h>
 
 int main() {
+
+    REGISTRO reg;
+
     PILHA pilha;
     inicializarPilha(&pilha);
-    // completarei com o código a partir daqui
+
+    // Inserindo elemento na pilha (base de teste)
+    reg.chave = 1;
+    inserirElementoPilha(&pilha, reg);
+    reg.chave = 2;
+    inserirElementoPilha(&pilha, reg);
+    reg.chave = 3;
+    inserirElementoPilha(&pilha, reg);
+
+    // Tentando imprimir a função implementada
+    exibirPilhaInvertida(&pilha);
+
+    // Difereça para função já existente
+    exibirPilha(&pilha);
 
     return 0;
 }
